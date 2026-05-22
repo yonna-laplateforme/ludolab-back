3b3cf062ebd1892ba49903e3be15f3e3c04583b8
Mon Nom <mon.email@exemple.com>
2026-05-22 15:18:25 +0200

    remise en form convention de nommage schema_prisma et crud ai_interaction

--- src/ai_interactions/ai_interactions.controller.ts
+++ src/ai_interactions/ai_interactions.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { AiInteractionsService } from './ai_interactions.service';
 
 @Controller('ai-interactions')
-export class AiInteractionsController {}
+export class AiInteractionsController {
+      constructor(private readonly AiInteractionsService: AiInteractionsService) {}
+      @Post()
+      create(
+        @Body()
+        createAiInteractionsDto: {
+          user_id: number;
+          exercise_id: number;
+          user_question: string;
+          ai_response: string;
+          help_type: string;
+        },
+      ) {
+        return this.AiInteractionsService.create(createAiInteractionsDto);
+      }
+    
+      @Get()
+      findAll() {
+        return this.AiInteractionsService.findAll();
+      }
+    
+      @Get(':id')
+      findOne(@Param('id') id: string) {
+        return this.AiInteractionsService.findOne(+id);
+      }
+    
+      @Delete(':id')
+      remove(@Param('id') id: string) {
+        return this.AiInteractionsService.remove(+id);
+      }
+}
