9d6c57eb104cc4bd0f03da191f3117d4da646dea
NicolasParvedyLaPlateforme <nicolas.parvedy@laplateforme.io>
2026-05-22 14:59:19 +0200

    correction fichier exercice

--- src/exercises/dto/update-exercise.dto.ts
+++ src/exercises/dto/update-exercise.dto.ts
@@ -1,4 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateExerciseDto } from './create-exercise.dto';
 
-export class UpdateUserDto extends PartialType(CreateExerciseDto) {}
+export class UpdateExerciceDto extends PartialType(CreateExerciseDto) {}
