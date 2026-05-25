52eb17c6c80408aae93cb2e22d50c8aeaa4bfc59
adrien-saccaggi <adrien.saccaggi@laplateforme.io>
2026-05-25 16:19:08 +0200

    c moi

--- src/subscriptions/subscriptions.controller.ts
+++ src/subscriptions/subscriptions.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import {Controller,Get,Post,Body,Param, Delete } from '@nestjs/common';
+import { SubscriptionsService } from './subscriptions.service';
+import { CreateSubscriptionDto } from './dto/create_subscription.dto';
 
-@Controller('subscriptions')
-export class SubscriptionsController {}
+@Controller('subscription')
+export class SubscriptionsController {
+  constructor(private readonly subscriptionsService: SubscriptionsService) {}
+  @Post()
+  create(
+    @Body()
+    createSubscriptionDto: CreateSubscriptionDto,
+  ) {
+    return this.subscriptionsService.create(createSubscriptionDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.subscriptionsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.subscriptionsService.findOne(+id);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.subscriptionsService.remove(+id);
+  }
+}
--- src/subscriptions/subscriptions.service.ts
+++ src/subscriptions/subscriptions.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class SubscriptionsService {}
+export class SubscriptionsService {
+    constructor(private prisma : PrismaService){}
+    async create(data:{
+        user_id: number;
+        plan_name:string;
+        is_active:boolean;
+        start_date:Date;
+        end_date:Date
+    }){
+        return this.prisma.subscription.create({
+            data: data,
+        })
+    }
+      async findAll() {
+    return this.prisma.subscription.findMany(); // ◄ Ajout du "s"
+  }
+
+  async findOne(id: number) {
+    return this.prisma.subscription.findUnique({
+      // ◄ Ajout du "s"
+      where: { id },
+    });
+  }
+
+  async remove(id: number) {
+    return this.prisma.subscription.delete({
+      // ◄ Ajout du "s"
+      where: { id },
+    });
+  }
+}
