28f1095f64fda695ae432ab6bf5b909cbed2945e
NicolasParvedyLaPlateforme <nicolas.parvedy@laplateforme.io>
2026-05-28 16:18:15 +0200

    Ajout de documentation et structure dto

--- src/exercises/dto/create-exercise-request.dto.ts
+++ src/exercises/dto/create-exercise-request.dto.ts
@@ -1,7 +1,12 @@
 import { IsString, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateExerciseRequestDto {
-  @IsString()
-  @IsNotEmpty()
-  url_image!: string;
+    @ApiProperty({ 
+        description: "L'URL de l'image de l'exercice à analyser par l'IA",
+        example: 'https://mon-stockage.com/image_exercice.png' // Fournit un exemple concret au Front !
+    })
+    @IsString()
+    @IsNotEmpty()
+    url_image!: string;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,8 +12,19 @@ async function bootstrap() {
     credentials: true,
   });
 
+  const config = new DocumentBuilder()
+    .setTitle('Ludolab API')
+    .setDescription('Documentation officielle de l\'API Ludolab avec intégration IA')
+    .setVersion('1.0')
+    .addTag('exercises') // Regroupe tes routes sous un tag
+    .build();
+
+  // 2. Génération du document et création du endpoint '/api'
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3001);
   console.log('🚀 Le serveur NestJS tourne sur : http://localhost:3001');
+  console.log('📖 La documentation Swagger est disponible sur : http://localhost:3001/api');
 }
 bootstrap();
