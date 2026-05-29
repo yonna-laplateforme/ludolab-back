55fa417e2119911b86aafd42ebc8e4bfdede8c15
adrien-saccaggi <adrien.saccaggi@laplateforme.io>
2026-05-29 11:17:52 +0200

    salut

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
   // Permet à ton front de se connecter à ton API (très utile pour la suite)
   app.enableCors({
-    origin: 'http://localhost:3001', 
+    origin: 'http://localhost:3000',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
-
 
   await app.listen(3001);
   console.log('🚀 Le serveur NestJS tourne sur : http://localhost:3001');
