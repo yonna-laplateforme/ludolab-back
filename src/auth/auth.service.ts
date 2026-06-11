0dcd056cd951f618f3168df19fcbf6b4dcd122b8
NicolasParvedyLaPlateforme <nicolas.parvedy@laplateforme.io>
2026-06-11 13:13:31 +0200

    back valide

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import * as bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
@@ -10,27 +10,22 @@ export class AuthService {
     private readonly UsersService: UsersService,
     private jwtService: JwtService,
     private configService: ConfigService,
-  ) {}
+  ) { }
 
   async signIn(identifier: string, password: string): Promise<any> {
-    console.log('OK');
     const user = await this.UsersService.findByIdentifier(identifier);
-    if (user != null) {
-      console.log(await bcrypt.compare(password, user.password));
-    }
-    if (user == null)
-    {
-      throw new NotFoundException("Identifiants incorrect")
+    if (user == null) {
+      throw new UnauthorizedException("Identifiants incorrect")
     }
     if (user && (await bcrypt.compare(password, user.password))) {
       const payload = {
         username: user.username,
         id: user.id,
       };
-      console.log(payload);
+
       return {
         access_tokken: this.jwtService.sign(payload, {
-          secret: this.configService.get<string>('JWT_SECRET'), // <-- On l'utilise ici
+          secret: this.configService.get<string>('JWT_SECRET'),
         }),
       };
     }
